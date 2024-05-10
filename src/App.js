dd8343e7e108bc134c74eb40c074e36392e17983
ABIRAM323 <abiclash231@gmail.com>
2024-05-10 11:23:05 +0530

    ok

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AddRecipe from './component/AddRecipe';
+
+function App() {
+  return (
+    <div>
+     <AddRecipe/>
+    </div>
+  );
+}
+
+export default App;
